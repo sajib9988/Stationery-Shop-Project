af5d202a180b3d854ace4b668711849aad7e03fb
sajib9988 <sajib.biswas9988@gmail.com>
2025-02-28 12:16:47 +0600

    u

--- Client/src/pages/OrderPage.tsx
+++ Client/src/pages/OrderPage.tsx
@@ -133,7 +133,8 @@ const OrderPage = () => {
           </div>
           <div className="w-full text-center mt-3">
             <h4 className="text-center w-full text-red-800 text-lg font-bold">!!Warning!!</h4>
-            <p>এডমিন order ভেরিফাই করার পর প্রডাক্ট কোয়ান্টিটি কমবে</p>
+            <p>
+            Product quantity will decrease after admin order verification After admin order verification</p>
           </div>
         </div>
       </div>
